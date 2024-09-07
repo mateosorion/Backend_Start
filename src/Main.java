851a292d5a2ab2c744b48e49aaed16b7fa28c030
mateosorion <mateo_osorion@soy.sena.edu.co>
2024-09-06 19:11:01 -0500

    Proyecto clase Libro

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner leer = new Scanner(System.in);
+        Libro Libro1 = new Libro(93445,"La familia adams",562,"Gabo",false);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Libro1.consultarLibro();
+        Libro1.prestar();
+        Libro1.consultarLibro();
+        Libro1.devolver();
+        Libro1.consultarLibro();
+        Libro1.imprimir(2.3);
+
+        Libro Libro2 = new Libro();
+
+        Libro2.setIsbn(4598);
+        Libro2.setTitulo("Juanito y su mujer");
+        Libro2.setNumPaginas(642);
+        Libro2.setAutor("Rafael Pombo");
+
+        Libro2.consultarLibro();
+        Libro2.prestar();
+        Libro2.consultarLibro();
+        Libro2.devolver();
+        Libro2.consultarLibro();
+        Libro2.imprimir(2.3);
+
+        Libro Libro3 = new Libro();
+
+        System.out.println("Ingrese el isbn del libro");
+        Libro3.setIsbn(leer.nextInt());
+        leer.nextLine();
+        System.out.println("Ingrese el titulo del libro");
+        Libro3.setTitulo(leer.nextLine());
+        System.out.println("Ingrese el numero de paginas del libro");
+        Libro3.setNumPaginas(leer.nextInt());
+        leer.nextLine();
+        System.out.println("Ingrese el autor del libro");
+        Libro3.setAutor(leer.nextLine());
+
+        Libro3.consultarLibro();
+        Libro3.prestar();
+        Libro3.consultarLibro();
+        Libro3.devolver();
+        Libro3.consultarLibro();
+        Libro3.imprimir(2.3);
+
+
         }
     }
-}
+
