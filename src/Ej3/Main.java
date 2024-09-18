05ea5580259bf6c91e29d7a02824487f6f8d79f8
mateosorion <mateo_osorion@soy.sena.edu.co>
2024-09-17 23:00:10 -0500

    Tercer ejercicio, generics

---
+++ src/Ej3/Main.java
@@ -0,0 +1,36 @@
+package Ej3;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Persona> personas = new ArrayList<>();
+
+        personas.add(new Persona("Juan", "Perez", "juanperez@gmail.com", 12345678, "DNI", 30,
+                1122334455, "Masculino", "Calle Falsa 123"));  //Agregar
+
+        personas.add(new Persona("Maria", "Gomez", "mariagomez@gmail.com", 87654321, "DNI", 25,
+                1122334455, "Femenino", "Calle Falsa 123"));  //Agregar
+
+        System.out.println("Persona: " + personas.get(0));  //Obtener
+        System.out.println("Persona: " + personas.get(1));  //Obtener
+
+        personas.remove(0);  //Eliminar
+
+        System.out.println("Persona: " + personas.get(0));  //Obtener
+
+        personas.add(0, new Persona("Pedro", "Gonzalez", "pedrogonzales@gmail.com", 12345678, "DNI", 30,
+                1122334455, "Masculino", "Calle Falsa 123"));  //Agregar en posicion especifica
+
+        System.out.println("Persona: " + personas.get(0));  //Obtener
+        System.out.println("Persona: " + personas.get(1));  //Obtener
+
+        personas.set(1, new Persona("Jose", "Romualdo", "joseromualdo@gmail.com", 12345678, "DNI", 30,
+                1122334455, "Masculino", "Calle Falsa 123"));  //Modificar
+
+        System.out.println("Persona: " + personas.get(0));  //Obtener
+        System.out.println("Persona: " + personas.get(1));  //Obtener
+
+
+    }
+}
---
+++ src/Ej3/Persona.java
@@ -0,0 +1,116 @@
+package Ej3;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private int dni;
+    private String tipoDni;
+    private int edad;
+    private int celular;
+    private String genero;
+    private String direccion;
+
+    public Persona(String nombre, String apellido, String email, int dni, String tipoDni, int edad, int celular, String genero, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.dni = dni;
+        this.tipoDni = tipoDni;
+        this.edad = edad;
+        this.celular = celular;
+        this.genero = genero;
+        this.direccion = direccion;
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getTipoDni() {
+        return tipoDni;
+    }
+
+    public void setTipoDni(String tipoDni) {
+        this.tipoDni = tipoDni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", dni=" + dni +
+                ", tipoDni='" + tipoDni + '\'' +
+                ", edad=" + edad +
+                ", celular=" + celular +
+                ", genero='" + genero + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+}
